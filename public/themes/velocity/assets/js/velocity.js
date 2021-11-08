d725b2baaf891f30345723e907cc7967f4bbb3d3
Muhammad Tayyab <tayyabkhurram62@gmail.com>
2021-11-09 00:26:38 +0500

    Updates

--- public/themes/velocity/assets/js/velocity.js
+++ public/themes/velocity/assets/js/velocity.js
@@ -26357,7 +26357,7 @@ var render = function() {
   return _c(
     "div",
     {
-      class: "dropdown8 " + (_vm.cartItems.length > 0 ? "" : "disable-active")
+      class: "dropdown9 " + (_vm.cartItems.length > 0 ? "" : "disable-active")
     },
     [
       _c("mini-cart-button", {
