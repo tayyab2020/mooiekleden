b5f251f2c220458e0a01b0c55130c4f1a9072185
Muhammad Tayyab <tayyabkhurram62@gmail.com>
2021-11-12 22:40:40 +0500

    Updates

--- packages/Webkul/Shop/src/Resources/views/products/view/attributes.blade.php
+++ packages/Webkul/Shop/src/Resources/views/products/view/attributes.blade.php
@@ -2,6 +2,58 @@
 
 {!! view_render_event('bagisto.shop.products.view.attributes.before', ['product' => $product]) !!}
 
+<div style="display: flex;">
+                                        
+                                        <div class="col-lg-4">
+                                        
+                                            @if ($product->getTypeInstance()->showQuantityBox())
+                                                <quantity-changer quantity-text="{{ __('shop::app.products.quantity') }}"></quantity-changer>
+                                            @else
+                                                <input type="hidden" name="quantity" value="1">
+                                            @endif
+
+                                        </div>
+
+                                        <div style="display: flex;flex-flow: wrap;" class="col-lg-8">
+
+                                            <div style="padding: 0;margin-bottom: 10px;" class="col-12 price">
+                                                @include ('shop::products.price', ['product' => $product])
+
+                                                @if (Webkul\Tax\Helpers\Tax::isTaxInclusive() && $product->getTypeInstance()->getTaxCategory())
+                                                    <span>
+                                                        {{ __('velocity::app.products.tax-inclusive') }}
+                                                    </span>
+                                                @endif
+                                            </div>
+
+                                            @if (count($product->getTypeInstance()->getCustomerGroupPricingOffers()) > 0)
+                                                <div class="col-12">
+                                                    @foreach ($product->getTypeInstance()->getCustomerGroupPricingOffers() as $offers)
+                                                        {{ $offers }} </br>
+                                                    @endforeach
+                                                </div>
+                                            @endif
+
+                                            <div class="product-actions">
+                                                @if (core()->getConfigData('catalog.products.storefront.buy_now_button_display'))
+                                                    @include ('shop::products.buy-now', [
+                                                        'product' => $product,
+                                                    ])
+                                                @endif
+
+                                                @include ('shop::products.add-to-cart', [
+                                                    'form' => false,
+                                                    'product' => $product,
+                                                    'showCartIcon' => false,
+                                                    'showCompare' => core()->getConfigData('general.content.shop.compare_option') == "1"
+                                                                ? true : false,
+                                                ])
+                                            </div>
+
+                                        </div>
+
+                                    </div>
+
 @if ($customAttributeValues = $productViewHelper->getAdditionalData($product))
     <accordian :title="'{{ __('shop::app.products.specification') }}'" :active="false">
         <div slot="header">
