70cae01efff52e2cd599f46b9852fe43368bf2a6
Muhammad Tayyab <tayyabkhurram62@gmail.com>
2021-11-14 01:49:51 +0500

    Updates

--- packages/Webkul/Velocity/src/Resources/views/shop/layouts/scripts.blade.php
+++ packages/Webkul/Velocity/src/Resources/views/shop/layouts/scripts.blade.php
@@ -1,6 +1,6 @@
 <script
     type="text/javascript"
-    src="{{ asset('themes/velocity/assets/js/velocity-core.js?id=2') }}">
+    src="{{ asset('themes/velocity/assets/js/velocity-core.js?id=30') }}">
 </script>
 
 <script type="text/javascript">
