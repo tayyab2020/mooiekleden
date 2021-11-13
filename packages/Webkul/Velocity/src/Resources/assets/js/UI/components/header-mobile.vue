3325345d784b2a7d7b6690e21e5347f9769477b1
Muhammad Tayyab <tayyabkhurram62@gmail.com>
2021-11-14 01:17:19 +0500

    Updates

--- packages/Webkul/Velocity/src/Resources/assets/js/UI/components/header-mobile.vue
+++ packages/Webkul/Velocity/src/Resources/assets/js/UI/components/header-mobile.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="row">
-        <div class="col-6">
+        <div style="display: flex;" class="col-7">
             <div v-if="hamburger" class="nav-container scrollable">
                 <div class="wrapper" v-if="this.rootCategories">
                     <div class="greeting drawer-section fw6">
@@ -300,7 +300,7 @@
             <slot name="logo"></slot>
         </div>
 
-        <div class="right-vc-header col-6">
+        <div class="right-vc-header col-5">
             <slot name="top-header"></slot>
 
             <a class="unset cursor-pointer" @click="openSearchBar">
