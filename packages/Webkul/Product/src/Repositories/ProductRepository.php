16e168f04f85ce14a7c473c671283e4259795e36
Muhammad Tayyab <tayyabkhurram62@gmail.com>
2021-11-26 02:05:34 +0500

    Updates

--- packages/Webkul/Product/src/Repositories/ProductRepository.php
+++ packages/Webkul/Product/src/Repositories/ProductRepository.php
@@ -233,7 +233,9 @@ class ProductRepository extends Repository
                             $qb->where(function ($qb) use ($priceRange){
                                 $qb
                                     ->where('variants.min_price', '>=',  core()->convertToBasePrice($priceRange[0]))
-                                    ->where('variants.min_price', '<=',  core()->convertToBasePrice(end($priceRange)));
+                                    ->where('variants.min_price', '<=',  core()->convertToBasePrice(end($priceRange)))
+                                    ->where('variants.size_label', '!=',  'Custom Size')
+-                                   ->where('variants.size_label', '!=',  'Maatwerk');
                             })
                             ->orWhere(function ($qb) use ($priceRange) {
                                 $qb
