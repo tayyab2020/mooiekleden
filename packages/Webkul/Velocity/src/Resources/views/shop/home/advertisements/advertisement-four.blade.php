aedbef33096675780eb8efadf546672ab78af982
Muhammad Tayyab <tayyabkhurram62@gmail.com>
2021-11-29 19:21:23 +0500

    Updates

--- packages/Webkul/Velocity/src/Resources/views/shop/home/advertisements/advertisement-four.blade.php
+++ packages/Webkul/Velocity/src/Resources/views/shop/home/advertisements/advertisement-four.blade.php
@@ -14,13 +14,20 @@
     @endphp
 
     @if ($advertisementFour)
+
         @php
             $isRendered = true;
+            $images_texts = json_decode($velocityMetaData->images_texts, true);
+            $images_urls = json_decode($velocityMetaData->images_urls, true);
         @endphp
+
+        <link href="https://kit-pro.fontawesome.com/releases/v5.15.4/css/pro.min.css" rel="stylesheet">
 
         <div class="container-fluid advertisement-four-container">
             <div class="row">
-                <div class="col-lg-4 col-12 advertisement-container-block no-padding">
+
+                <div style="position: relative;" class="col-lg-4 col-12 advertisement-container-block no-padding">
+                    
                     @if (isset($advertisementFour[0]))
                         <a @if (isset($one)) href="{{ $one }}" @endif aria-label="Advertisement">
                             <img
@@ -28,40 +35,116 @@
                                 @if (! $isLazyLoad) src="{{ asset('/storage/' . $advertisementFour[0]) }}" @endif
                                 data-src="{{ asset('/storage/' . $advertisementFour[0]) }}" alt="" />
                         </a>
+
+                        @if($images_texts[0])
+
+                            <a style="position: absolute;bottom: 0;left: 0;width: 100%;color: black;text-decoration: none !important;" href="{{ $images_urls[0] ? $images_urls[0] : url('/') }}">
+                            
+                                <div style="width: 100%;height: 50px;display: flex;justify-content: center;">
+                                    <div style="width: 80%;height: 50px;background-color: white;box-shadow: 0px 0px 6px 2px #8d8d8d52;padding: 10px;display: flex;align-items: center;justify-content: center;">
+                                        <div><span style="font-size: 20px;font-weight: 600;">{{ $images_texts[0] }}</span> <i style="font-size: 14px;margin-left: 5px;" class="fas fa-chevron-right"></i></div>
+                                    </div>
+                                </div>
+
+                            </a>
+
+                        @endif
+
                     @endif
+
                 </div>
 
                 <div class="col-lg-4 col-12 advertisement-container-block offers-ct-panel">
+                    
                     @if (isset($advertisementFour[1]))
-                        <a @if (isset($two)) href="{{ $two }}" @endif class="row col-12 remove-padding-margin" aria-label="Advertisement">
-                            <img
-                                class="offers-ct-top {{ $isLazyLoad ? 'lazyload' : '' }}"
-                                @if (! $isLazyLoad) src="{{ asset('/storage/' . $advertisementFour[1]) }}" @endif
-                                data-src="{{ asset('/storage/' . $advertisementFour[1]) }}" alt="" />
-                        </a>
+
+                        <div style="position: relative;">
+
+                            <a @if (isset($two)) href="{{ $two }}" @endif class="row col-12 remove-padding-margin" aria-label="Advertisement">
+                                <img
+                                    class="offers-ct-top {{ $isLazyLoad ? 'lazyload' : '' }}"
+                                    @if (! $isLazyLoad) src="{{ asset('/storage/' . $advertisementFour[1]) }}" @endif
+                                    data-src="{{ asset('/storage/' . $advertisementFour[1]) }}" alt="" />
+                            </a>
+
+                            @if($images_texts[1])
+
+                                <a style="position: absolute;bottom: 0;left: 0;width: 100%;color: black;text-decoration: none !important;" href="{{ $images_urls[1] ? $images_urls[1] : url('/') }}">
+                            
+                                    <div style="width: 100%;height: 50px;display: flex;justify-content: center;">
+                                        <div style="width: 80%;height: 50px;background-color: white;box-shadow: 0px 0px 6px 2px #8d8d8d52;padding: 10px;display: flex;align-items: center;justify-content: center;">
+                                            <div><span style="font-size: 20px;font-weight: 600;">{{ $images_texts[1] }}</span> <i style="font-size: 14px;margin-left: 5px;" class="fas fa-chevron-right"></i></div>
+                                        </div>
+                                    </div>
+
+                                </a>
+
+                            @endif
+
+                        </div>
+
                     @endif
 
                     <div style="height: 10px;"></div>
 
                     @if (isset($advertisementFour[2]))
-                        <a @if (isset($three)) href="{{ $three }}" @endif class="row col-12 remove-padding-margin" aria-label="Advertisement">
-                            <img
-                                class="offers-ct-bottom {{ $isLazyLoad ? 'lazyload' : '' }}"
-                                @if (! $isLazyLoad) src="{{ asset('/storage/' . $advertisementFour[2]) }}" @endif
-                                data-src="{{ asset('/storage/' . $advertisementFour[2]) }}" alt="" />
-                        </a>
+
+                        <div style="position: relative;">
+
+                            <a @if (isset($three)) href="{{ $three }}" @endif class="row col-12 remove-padding-margin" aria-label="Advertisement">
+                                <img
+                                    class="offers-ct-bottom {{ $isLazyLoad ? 'lazyload' : '' }}"
+                                    @if (! $isLazyLoad) src="{{ asset('/storage/' . $advertisementFour[2]) }}" @endif
+                                    data-src="{{ asset('/storage/' . $advertisementFour[2]) }}" alt="" />
+                            </a>
+
+                            @if($images_texts[2])
+
+                                <a style="position: absolute;bottom: 0;left: 0;width: 100%;color: black;text-decoration: none !important;" href="{{ $images_urls[2] ? $images_urls[2] : url('/') }}">
+                            
+                                    <div style="width: 100%;height: 50px;display: flex;justify-content: center;">
+                                        <div style="width: 80%;height: 50px;background-color: white;box-shadow: 0px 0px 6px 2px #8d8d8d52;padding: 10px;display: flex;align-items: center;justify-content: center;">
+                                            <div><span style="font-size: 20px;font-weight: 600;">{{ $images_texts[2] }}</span> <i style="font-size: 14px;margin-left: 5px;" class="fas fa-chevron-right"></i></div>
+                                        </div>
+                                    </div>
+
+                                </a>
+
+                            @endif
+
+                        </div>
+                    
                     @endif
+
                 </div>
 
-                <div class="col-lg-4 col-12 advertisement-container-block no-padding">
+                <div style="position: relative;" class="col-lg-4 col-12 advertisement-container-block no-padding">
+
                     @if (isset($advertisementFour[3]))
+                        
                         <a @if (isset($four)) href="{{ $four }}" @endif aria-label="Advertisement">
                             <img
                                 class="{{ $isLazyLoad ? 'lazyload' : '' }}"
                                 @if (! $isLazyLoad) src="{{ asset('/storage/' . $advertisementFour[3]) }}" @endif
                                 data-src="{{ asset('/storage/' . $advertisementFour[3]) }}" alt="" />
                         </a>
+                        
+                        @if($images_texts[3])
+
+                            <a style="position: absolute;bottom: 0;left: 0;width: 100%;color: black;text-decoration: none !important;" href="{{ $images_urls[3] ? $images_urls[3] : url('/') }}">
+                            
+                                <div style="width: 100%;height: 50px;display: flex;justify-content: center;">
+                                    <div style="width: 80%;height: 50px;background-color: white;box-shadow: 0px 0px 6px 2px #8d8d8d52;padding: 10px;display: flex;align-items: center;justify-content: center;">
+                                        <div><span style="font-size: 20px;font-weight: 600;">{{ $images_texts[3] }}</span> <i style="font-size: 14px;margin-left: 5px;" class="fas fa-chevron-right"></i></div>
+                                    </div>
+                                </div>
+
+                            </a>
+
+                        @endif
+
                     @endif
+
                 </div>
             </div>
         </div>
